d6f99f19644c6a9591e3db5974b4492595f477ed
Joscha <joscha@schnipseljagd.org>
2016-06-17 17:52:08 +0200

    fix test

--- tests/Test/Prometheus/GaugeTest.php
+++ tests/Test/Prometheus/GaugeTest.php
@@ -3,6 +3,7 @@
 
 namespace Test\Prometheus;
 use PHPUnit_Framework_TestCase;
+use Prometheus\Gauge;
 
 /**
  * See https://prometheus.io/docs/instrumenting/exposition_formats/
@@ -17,7 +18,7 @@ class cc_Status_Prometheus_GaugeTest extends PHPUnit_Framework_TestCase
         $gauge = new Gauge('test', 'some_metric', 'this is for testing', array('foo', 'bar'));
         $gauge->set(123, array('foo' => 'lalal', 'bar' => 'lululu'));
         $this->assertThat(
-            $gauge->getMetricLabelVariations(),
+            $gauge->getSamples(),
             $this->equalTo(
                 array(
                     array(
@@ -26,7 +27,8 @@ class cc_Status_Prometheus_GaugeTest extends PHPUnit_Framework_TestCase
                             array('name' => 'foo', 'value' => 'lalal'),
                             array('name' => 'bar', 'value' => 'lululu')
                         ),
-                        'value' => 123
+                        'value' => 123,
+                        'help' => 'this is for testing'
                     )
                 )
             )
@@ -41,13 +43,14 @@ class cc_Status_Prometheus_GaugeTest extends PHPUnit_Framework_TestCase
         $gauge = new Gauge('test', 'some_metric', 'this is for testing');
         $gauge->set(123);
         $this->assertThat(
-            $gauge->getMetricLabelVariations(),
+            $gauge->getSamples(),
             $this->equalTo(
                 array(
                     array(
                         'name' => 'test_some_metric',
                         'labels' => array(),
-                        'value' => 123
+                        'value' => 123,
+                        'help' => 'this is for testing'
                     )
                 )
             )
