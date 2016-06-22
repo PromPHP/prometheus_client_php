beeea83ea98f8912f0122e417de1922d24a1107c
Joscha <joscha@schnipseljagd.org>
2016-06-22 15:13:59 +0200

    Use a sha1 as key.

--- src/Prometheus/Sample.php
+++ src/Prometheus/Sample.php
@@ -55,6 +55,6 @@ class Sample
      */
     public function getKey()
     {
-        return $this->getName() . serialize($this->getLabelValues());
+        return sha1($this->getName() . serialize($this->getLabelValues()));
     }
 }
