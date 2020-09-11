5a01128bd2c1dc098bad587012368c7bc9b69e26
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2020-09-11 13:49:31 +0200

    Move to Github Actions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,4 +13,4 @@ if (!file_exists($autoload)) {
 $loader = require $autoload;
 $loader->add('Test\\Prometheus', __DIR__);
 
-define('REDIS_HOST', isset($_SERVER['REDIS_HOST']) ? $_SERVER['REDIS_HOST'] : '127.0.0.1');
+define('REDIS_HOST', isset($_ENV['REDIS_HOST']) ? $_ENV['REDIS_HOST'] : '127.0.0.1');
