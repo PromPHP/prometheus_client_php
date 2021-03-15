c61181ac603ce3039a25a80b2e88ce6f108f1636
Gregor Kralik <g.kralik@gmail.com>
2021-03-15 14:37:49 +0100

    add a test for custom APC prefixes
    Signed-off-by: Gregor Kralik <g.kralik@gmail.com>

--- tests/Test/Prometheus/Storage/APCTest.php
+++ tests/Test/Prometheus/Storage/APCTest.php
@@ -41,4 +41,29 @@ class APCTest extends TestCase
             ])
         );
     }
+
+    /**
+     * @test
+     */
+    public function itShouldUseConfiguredPrefix()
+    {
+        $apc = new APC('custom_prefix');
+        $apc->wipeStorage();
+
+        $registry = new CollectorRegistry($apc);
+
+        $registry->getOrRegisterCounter('namespace', 'counter', 'counter help')->inc();
+        $registry->getOrRegisterGauge('namespace', 'gauge', 'gauge help')->inc();
+        $registry->getOrRegisterHistogram('namespace', 'histogram', 'histogram help')->observe(1);
+
+        $entries = iterator_to_array(new APCUIterator('/^custom_prefix:.*:meta$/'), true);
+
+        $cacheKeys = array_map(function ($item) {
+            return $item['key'];
+        }, $entries);
+
+        self::assertArrayHasKey('custom_prefix:counter:namespace_counter:meta', $cacheKeys);
+        self::assertArrayHasKey('custom_prefix:gauge:namespace_gauge:meta', $cacheKeys);
+        self::assertArrayHasKey('custom_prefix:histogram:namespace_histogram:meta', $cacheKeys);
+    }
 }
