35d5a68628ea18209938bc1b8796646015ab93cf
Janez Urevc <janez@janezurevc.name>
2024-08-05 09:58:08 +0200

    Add PDO storage adapter.  (#157)
    * PDO storage adapter - counter.
    
    Signed-off-by: Janez Urevc <janez@tag1.com>
    Co-authored-by: Lukas Kämmerling <github@lukas-kaemmerling.de>

---
+++ tests/Test/Prometheus/PDO/CounterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Prometheus\PDO;
+
+use Prometheus\Storage\PDO;
+use Test\Prometheus\AbstractCounterTest;
+
+/**
+ * See https://prometheus.io/docs/instrumenting/exposition_formats/
+ */
+class CounterTest extends AbstractCounterTest
+{
+    use PdoCredentialsTrait;
+
+    /**
+     * @var \PDO|null
+     */
+    private $pdo;
+
+    public function configureAdapter(): void
+    {
+        $this->pdo = new \PDO($this->getDsn(), $this->getUsername(), $this->getPassword());
+        $prefix = 'test' . substr(hash('sha256', uniqid()), 0, 6) . '_';
+        $this->adapter = new PDO($this->pdo, $prefix);
+        $this->adapter->wipeStorage();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->adapter->deleteTables(); /** @phpstan-ignore-line */
+        $this->adapter = null; /** @phpstan-ignore-line */
+        $this->pdo = null;
+    }
+}
