4f71eb1dd5a7df0466cd78a961e992042fd7f29b
SevaWeb011 <49000654+SevaWeb011@users.noreply.github.com>
2025-03-03 22:33:03 +0200

    Catch invalid JSON from Redis and throw an error (#174)
    * Catch invalid JSON from Redis and throw an error
    
    Signed-off-by: Vsevolod <vmalygin@adsterra.com>
    
    * was added a custom json exception for metrics
    
    Signed-off-by: Vsevolod <vmalygin@adsterra.com>
    
    * refactoring
    
    Signed-off-by: Vsevolod <vmalygin@adsterra.com>
    
    * Update src/Prometheus/Exception/MetricJsonException.php
    
    ---------
    
    Signed-off-by: Vsevolod <vmalygin@adsterra.com>
    Co-authored-by: Vsevolod <vmalygin@adsterra.com>
    Co-authored-by: Lukas Kämmerling <github@lukas-kaemmerling.de>
    Co-authored-by: Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>

--- src/Prometheus/Storage/RedisNg.php
+++ src/Prometheus/Storage/RedisNg.php
@@ -6,6 +6,7 @@ namespace Prometheus\Storage;
 
 use InvalidArgumentException;
 use Prometheus\Counter;
+use Prometheus\Exception\MetricJsonException;
 use Prometheus\Exception\StorageException;
 use Prometheus\Gauge;
 use Prometheus\Histogram;
@@ -405,6 +406,7 @@ LUA
 
     /**
      * @return mixed[]
+     * @throws MetricJsonException
      */
     private function collectHistograms(): array
     {
@@ -427,6 +429,10 @@ LUA
                     continue;
                 }
                 $allLabelValues[] = $d['labelValues'];
+            }
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                $this->throwMetricJsonException($key);
             }
 
             // We need set semantics.
@@ -599,6 +605,9 @@ LUA
                     'labelValues' => json_decode($k, true),
                     'value' => $value,
                 ];
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    $this->throwMetricJsonException($key, $gauge['name']);
+                }
             }
 
             if ($sortMetrics) {
@@ -614,6 +623,7 @@ LUA
 
     /**
      * @return mixed[]
+     * @throws MetricJsonException
      */
     private function collectCounters(bool $sortMetrics = true): array
     {
@@ -632,6 +642,9 @@ LUA
                     'labelValues' => json_decode($k, true),
                     'value' => $value,
                 ];
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    $this->throwMetricJsonException($key, $counter['name']);
+                }
             }
 
             if ($sortMetrics) {
@@ -702,5 +715,18 @@ LUA
             throw new RuntimeException(json_last_error_msg());
         }
         return $decodedValues;
+    }
+
+    /**
+     * @param string $redisKey
+     * @param string|null $metricName
+     * @return void
+     * @throws MetricJsonException
+     */
+    private function throwMetricJsonException(string $redisKey, ?string $metricName = null): void
+    {
+        $metricName = $metricName ?? 'unknown';
+        $message = 'Json error: ' . json_last_error_msg() . ' redis key : ' . $redisKey . ' metric name: ' . $metricName;
+        throw new MetricJsonException($message, 0, null, $metricName);
     }
 }
