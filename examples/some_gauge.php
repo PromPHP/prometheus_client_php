a3c7a3e0963cc839ee44d9b68168ceea2ccb4973
Mateusz Cholewka <mateusz@cholewka.com.pl>
2025-05-01 00:12:40 +0200

    tests: add config for blackbox test
    Signed-off-by: Mateusz Cholewka <mateusz@cholewka.com.pl>

--- examples/some_gauge.php
+++ examples/some_gauge.php
@@ -5,7 +5,6 @@ require __DIR__ . '/../vendor/autoload.php';
 use Prometheus\CollectorRegistry;
 use Prometheus\Storage\Redis;
 
-
 error_log('c=' . $_GET['c']);
 
 $adapter = $_GET['adapter'];
@@ -13,6 +12,8 @@ $adapter = $_GET['adapter'];
 if ($adapter === 'redis') {
     Redis::setDefaultOptions(['host' => $_SERVER['REDIS_HOST'] ?? '127.0.0.1']);
     $adapter = new Prometheus\Storage\Redis();
+} elseif ($adapter === 'predis') {
+    $adapter = new Prometheus\Storage\Predis(['host' => $_SERVER['REDIS_HOST'] ?? '127.0.0.1']);
 } elseif ($adapter === 'apc') {
     $adapter = new Prometheus\Storage\APC();
 } elseif ($adapter === 'apcng') {
